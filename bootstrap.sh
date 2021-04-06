25f37c8deef7c1954ef679d48b0aea56df1f885a
Gunther Laure <gunther.laure@dewetron.com>
2021-04-06 13:30:16 +0200

    Update bootstrap.sh
    Use --yes for apt

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,10 +10,10 @@ set -e
 #
 
 # Assure python3 is installed and /usr/bin/python is a symlink to python3
-apt install python-is-python3
+apt install --yes python-is-python3
 
 # Pip3 and build essentials are needed
-apt install python3-pip build-essential
+apt install --yes python3-pip build-essential
 
 # Install PyQt5
 pip3 install pyqt5
@@ -23,11 +23,11 @@ pip3 install mercurial==5.4
 
 # Install Qscintilla.
 pip3 install qscintilla
-apt install pyqt5.qsci-dev
-apt install python3-pyqt5.qsci
+apt install --yes pyqt5.qsci-dev
+apt install --yes python3-pyqt5.qsci
 
 # iniparse
-apt install python3-iniparse
+apt install --yes python3-iniparse
 
 # done
 echo ---------------------------
