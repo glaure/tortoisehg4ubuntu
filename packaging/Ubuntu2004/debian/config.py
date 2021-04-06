cbb11af94208a3ef9c811a38f04873153ac896ec
Gunther Laure <gunther.laure@dewetron.com>
2021-04-06 16:25:03 +0200

    Provide config.py and icons

---
+++ packaging/Ubuntu2004/debian/config.py
@@ -0,0 +1,6 @@
+bin_path     = '/usr/bin'
+license_path = "/usr/share/common-licenses/GPL-2"
+locale_path  = '/usr/share/locale'
+icon_path    = '/usr/share/pixmaps/tortoisehg/icons'
+nofork       = True
+qt_api       = 'PyQt5'
